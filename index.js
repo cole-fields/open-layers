4a0a007db6b783e105a21c535d9a9787260bd6ae
Fields <FIELDSC@dfo-mpo.ca>
2019-10-31 16:03:45 -0700

    new vars

--- index.js
+++ index.js
@@ -2,24 +2,29 @@ import 'ol/ol.css';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
-import {fromLonLat} from 'ol/proj'; // the fromLonLat function
+// the fromLonLat function
+import {fromLonLat} from 'ol/proj';
 
-const bc = [-119.573568, 49.344938]; // longitude first, then latitude
+// longitude first, then latitude
+const bc = [-119.573568, 49.344938];
 // since we are using OSM, we have to transform the coordinates...
 const bcWebMercator = fromLonLat(bc);
 
+// base OSM layer
+const base new TileLayer({
+  source: new OSM()
+});
+
+// reefs WMS layer from geoserver
+const reefs new OpenLayers.Layer.WMS(
+  "Layer Reefs",
+  "http://34.217.47.31:8080/geoserver/cite/cite",
+  {layers: 'cite:reefs'}
+);
+
 const map = new Map({
   target: 'map',
-  layers: [
-    new TileLayer({
-      source: new OSM()
-    }),
-    new OpenLayers.Layer.WMS(
-      "Layer Reefs",
-      "http://34.217.47.31:8080/geoserver/wms/cite",
-      {layers: 'cite:reefs'}
-    )
-  ],
+  layers: [base, reefs],
   view: new View({
     center: bcWebMercator,
     zoom: 12
