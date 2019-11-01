99dcd3d2cc2ab3164ad7369472be0ef3db5acccf
cole-fields <cole.fields@dfo-mpo.gc.ca>
2019-11-01 10:16:24 -0700

    simplified new Map and added WMS sponge reef layer

--- index.js
+++ index.js
@@ -6,25 +6,23 @@ import OSM from 'ol/source/OSM';
 import {fromLonLat} from 'ol/proj';
 
 // longitude first, then latitude
-const bc = [-119.573568, 49.344938];
+const bc = [-123.375320, 49.421197];
 // since we are using OSM, we have to transform the coordinates...
 const bcWebMercator = fromLonLat(bc);
 
-// base OSM layer
-const base = new TileLayer({
-  source: new OSM()
-});
-
-// reefs WMS layer from geoserver
-const reefs = new OpenLayers.Layer.WMS(
-  "Layer Reefs",
-  "http://34.217.47.31:8080/geoserver/cite/cite",
-  {layers: 'cite:reefs'}
-);
-
 const map = new Map({
   target: 'map',
-  layers: [base, reefs],
+  layers: [
+    // openstreetmap base TileLayer
+    new TileLayer({
+      source: new OSM()
+    }),
+    // sponge reefs WMS layer from geoserver
+    new OpenLayers.Layer.WMS("Sponge Reefs", "http://52.32.75.54:8080/geoserver/cite/wms?service=WMS&version=1.1.0&request=GetMap",
+    {
+      layers: 'cite:reefs'
+    })
+  ],
   view: new View({
     center: bcWebMercator,
     zoom: 12
