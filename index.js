5740ac1e6c20e91b696d4e7b05cbc0ec2d5679df
cole-fields <cole.fields@dfo-mpo.gc.ca>
2019-11-01 15:13:10 -0700

    wfs troubleshooting

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import XYZ from 'ol/source/XYZ';
 import GeoJSON from 'ol/format/GeoJSON';
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
+import {bbox as bboxStrategy} from 'ol/loadingstrategy';
 
 // longitude first, then latitude
 var bc = [-123.375320, 49.421197];
@@ -19,10 +20,13 @@ var layers = [
   new VectorLayer({
     source: new VectorSource({
       format: new GeoJSON(),
-      url:'http://52.32.75.54:8080/geoserver/cite/ows',
-      params: {'LAYERS': 'cite:reefs'},
-      projection: 'EPSG:4326',
-      serverType: 'geoserver'
+      url: function(extent) {
+        return 'http://52.32.75.54:8080/geoserver/cite/ows?service=WFS&' +
+        'version=1.0.0&request=GetFeature&typeName=cite:reefs&' +
+        'maxFeatures=50&outputFormat=application%2Fjson' +
+        'bbox=' + extent.join(',') + ',EPSG:4326';
+      },
+      strategy: bboxStrategy
     })
   })
 ];
