20e35c728276bcca48b7aebe8d3782a4e2bef0d7
cole-fields <cole.fields@dfo-mpo.gc.ca>
2019-11-01 12:19:39 -0700

    testing wmts

--- index.js
+++ index.js
@@ -2,35 +2,37 @@ import 'ol/ol.css';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
+import TileWMS from 'ol/source/TileWMS';
 // the fromLonLat function
-import {fromLonLat} from 'ol/proj';
+// import {fromLonLat} from 'ol/proj';
 
 // longitude first, then latitude
-let bc = [-123.375320, 49.421197];
+var bc = [-123.375320, 49.421197];
 
-// main map variable specifying openstreetmap base in EPSG:4326
-let map = new Map({
-  target: 'map',
-  layers: [
-    // openstreetmap base TileLayer
-    new TileLayer({
-      source: new OSM()
+// layers
+var layers = [
+  new TileLayer({
+    source: new OSM()
+  }),
+  new TileLayer({
+    extent: [-123.469779968262, 49.3335990905762, -123.243202209473, 49.4680480957031],
+    source: new TileWMS({
+      url: 'http://52.32.75.54:8080/geoserver/cite/wms',
+      params: {'LAYERS': 'cite:reefs', 'TILED': true},
+      serverType: 'geoserver',
+      // Countries have transparency, so do not fade tiles:
+      transition: 0
     })
-  ],
+  })
+];
+
+// map
+var map = new Map({
+  layers: layers,
+  target: 'map',
   view: new View({
     projection: 'EPSG:4326',
     center: bc,
-    zoom: 12
+    zoom: 10
   })
 });
-
-// reefs WMS
-let reefs = new OpenLayers.Layer.WMS(
-  "Sponge Reefs", "http://52.32.75.54:8080/geoserver/cite/wms",
-  {
-    layers: 'cite:reefs'
-  }
-);
-
-// add reefs layer to map
-map.addLayer(reefs);
