280bc6443c31afdaaee509f1fd1b246631121d1f
cole-fields <cole.fields@dfo-mpo.gc.ca>
2019-11-01 14:49:20 -0700

    newvectorlayer

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import 'ol/ol.css';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
 import XYZ from 'ol/source/XYZ';
+import GeoJSON from 'ol/format/GeoJSON';
 import VectorLayer from 'ol/layer/Vector';
-import {Vector} from 'ol/source';
-import {GeoJSON} from 'ol/format';
+import VectorSource from 'ol/source/Vector';
 
 // longitude first, then latitude
 var bc = [-123.375320, 49.421197];
@@ -17,9 +16,10 @@ var layers = [
           url: 'http://basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png'
         })
       }),
-  new Vector({
-    source: new GeoJSON({
-      url: 'http://52.32.75.54:8080/geoserver/cite/ows',
+  new VectorLayer({
+    source: new VectorSource({
+      format: new GeoJSON(),
+      url:'http://52.32.75.54:8080/geoserver/cite/ows',
       params: {'LAYERS': 'cite:reefs'},
       projection: 'EPSG:4326',
       serverType: 'geoserver'
