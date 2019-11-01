f4de53817019c06fbc2bd530465258627617a1a6
cole-fields <cole.fields@dfo-mpo.gc.ca>
2019-11-01 14:11:53 -0700

    trying to add reefs as vector layer

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import XYZ from 'ol/source/XYZ';
-import TileWMS from 'ol/source/TileWMS';
-// the fromLonLat function
-// import {fromLonLat} from 'ol/proj';
+import VectorLayer from 'ol/layer/Vector';
+import {Vector} from 'ol/source';
+import {GeoJSON} from 'ol/format';
 
 // longitude first, then latitude
 var bc = [-123.375320, 49.421197];
@@ -17,14 +17,12 @@ var layers = [
           url: 'http://basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png'
         })
       }),
-  new TileLayer({
-    extent: [-123.469779968262, 49.3335990905762, -123.243202209473, 49.4680480957031],
-    source: new TileWMS({
-      url: 'http://52.32.75.54:8080/geoserver/cite/wms',
-      params: {'LAYERS': 'cite:reefs', 'TILED': true},
-      serverType: 'geoserver',
-      // Countries have transparency, so do not fade tiles:
-      transition: 0
+  new Vector({
+    source: new GeoJSON({
+      url: 'http://52.32.75.54:8080/geoserver/cite/ows',
+      params: {'LAYERS': 'cite:reefs'},
+      projection: 'EPSG:4326',
+      serverType: 'geoserver'
     })
   })
 ];
