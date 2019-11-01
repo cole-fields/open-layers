14b53dd9320e342e9ad416f6ba1bc73ba12dadd8
cole-fields <cole.fields@dfo-mpo.gc.ca>
2019-11-01 11:50:49 -0700

    wms attempt

--- index.js
+++ index.js
@@ -6,22 +6,16 @@ import OSM from 'ol/source/OSM';
 import {fromLonLat} from 'ol/proj';
 
 // longitude first, then latitude
-var bc = [-123.375320, 49.421197];
+let bc = [-123.375320, 49.421197];
 
 // main map variable specifying openstreetmap base in EPSG:4326
-var map = new Map({
+let map = new Map({
   target: 'map',
   layers: [
     // openstreetmap base TileLayer
     new TileLayer({
       source: new OSM()
-    }),
-    new OpenLayers.Layer.WMS(
-      "Sponge Reefs", "http://52.32.75.54:8080/geoserver/cite/wms",
-      {
-        layers: 'cite:reefs'
-      }
-    )
+    })
   ],
   view: new View({
     projection: 'EPSG:4326',
@@ -29,3 +23,14 @@ var map = new Map({
     zoom: 12
   })
 });
+
+// reefs WMS
+let reefs = new OpenLayers.Layer.WMS(
+  "Sponge Reefs", "http://52.32.75.54:8080/geoserver/cite/wms",
+  {
+    layers: 'cite:reefs'
+  }
+);
+
+// add reefs layer to map
+map.addLayer(reefs);
